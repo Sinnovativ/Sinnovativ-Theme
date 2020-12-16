97b45ce3a8289e0b4c154d9cf1dee5474c22dff7
Martin <martin.hertig@gmail.com>
2020-12-16 15:55:44 +0100

    add custom fields to start page

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -23,11 +23,11 @@ $container = get_theme_mod( 'understrap_container_type' );
   <div class="post-image home-image" style="background-image:url(<?php echo get_the_post_thumbnail_url( $post->ID, 'full' ); ?>)">
   <div class="row cta d-flex align-items-center">
     <div class="col-12 col-md-6 pb-2 pb-md-0">
-      <?php the_field('cta'); ?>
+      <?php  _e( 'Donate your bike for more perspectives in Africa.', 'Velafrica Startseite');  ?>
     </div>
     <div class="col-12 col-md-6 d-flex justify-content-end">
-      <a href="<?php the_field('button_1_link'); ?>" class="btn btn-secondary mr-2"><?php the_field('button_1_text'); ?></a>
-      <a href="<?php the_field('button_2_link'); ?>" class="btn btn-secondary"><?php the_field('button_2_text'); ?></a>
+      <a href="/wie-sie-helfen/velospende/" class="btn btn-secondary mr-2"><?php _e( 'Find a Collection Location', 'Velafrica Startseite');  ?></a>
+      <a href="/wie-sie-helfen/geldspende/" class="btn btn-secondary"><?php _e( 'Donate', 'Velafrica Startseite'); ?></a>
     </div>
   </div>
   </div>
@@ -159,7 +159,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
         <div class="row">
           <div class="col text-center pb-4">
-            <a href="was-wir-tun/news-events/" class="btn btn-outline-secondary"><?php the_field('mehr_news_button_text'); ?></a>
+            <a href="was-wir-tun/news-events/" class="btn btn-outline-secondary"><?php _e( 'More News', 'Velafrica Startseite');  ?></a>
           </div>
         </div>
 
