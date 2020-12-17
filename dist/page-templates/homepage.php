00c95ced815d38054b4fc2c5291be4c876a18a9c
Dino Meyer <dinomeyer@glunz.ch>
2020-12-17 14:14:09 +0100

    adding more mobile stuff

--- dist/page-templates/homepage.php
+++ dist/page-templates/homepage.php
@@ -178,3 +178,210 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- #full-width-page-wrapper end -->
 
 <?php get_footer(); ?>
+
+
+
+<script>
+( function( $ ) {
+
+
+ScrollTrigger.matchMedia({
+  // desktop
+  "(min-width: 640px)": function() {
+
+        ////////////////////////////
+        // Kennzahl Animation
+        ////////////////////////////
+
+        let kennzahlTl = gsap.timeline({
+            defaults:{ duration: 1.8 },
+            scrollTrigger: {
+                markers: false,
+                trigger: '#kennzahlen',
+                start: "center 90%",
+                end: '+=150px',
+                toggleActions: "play none none reverse",
+                onEnter: () => {
+                  kennzahlTl.timeScale(1.0);
+                },        
+                onEnterBack: () => {
+                  kennzahlTl.timeScale(4.0);
+                },
+            }
+        });
+        
+        kennzahlTl
+            .from(".kennzahl-img", { scale: 0.8, ease: "back.out(6)", stagger: { from: "center", amount: 0.25 } })
+            .from(".kennzahl-text-wrapper", { autoAlpha: 0, y: 40, ease: "power4.out", stagger: { from: "center", amount: 0.25 } }, "-=1.6")
+            .from(".number", { duration: 3, marginBottom: 30, innerHTML: "0", roundProps: "innerHTML", ease: "power4.out", stagger: { from: "center", amount: 0.25 } }, "-=2")    
+
+
+        ////////////////////////////
+        // Wikungskette Animation
+        ////////////////////////////
+        
+        // Path Animation
+        var wirkungsketteTl = gsap.timeline({defaults: {duration: 1, repeat:0},
+            scrollTrigger: {
+                markers: false,
+                trigger: "#wirkungskette",
+                scrub: 0.5,
+                start: "top 55%",
+                end: "bottom 65%",
+            }
+        })
+        
+        var d = 1;
+        var maxI = 0;
+        $($("#wirkungskette-svg g#path").children().get().reverse()).each(function(i, child){
+          wirkungsketteTl.to($(child), d, {"opacity":1});
+        });
+        
+
+        // Icons Animation
+        let iconAniTime = 1;
+        let textAniTime = 1.2;
+        
+        let wirkungsIconTl1 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "top 70%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl1.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl1.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-1", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-1", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl2 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', markers: false, start: "25% 66%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl2.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl2.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-2", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-2", { duration: textAniTime, /*x: 200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl3 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "37% 58%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl3.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl3.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-3", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-3", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl4 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "60% 69%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl4.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl4.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-4", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-4", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl5 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "71% 52%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl5.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl5.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-5", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-5", { duration: textAniTime, /*x: 200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl6 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "bottom 66%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl6.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl6.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-6", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)"})
+        .from(".wk-text-box-6", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" )
+
+
+  },
+  // mobile
+  "(max-width: 639px)": function() {
+
+        ////////////////////////////
+        // Kennzahl Animation
+        ////////////////////////////    
+
+        let kennzahlTl1 = gsap.timeline({
+            defaults:{ duration: 1.8 },
+            scrollTrigger: { trigger: '.kennzahl-1', start: "center 90%", end: '+=150px', toggleActions: "play none none reverse", onEnter: () => { kennzahlTl1.timeScale(1.0); }, onEnterBack: () => { kennzahlTl1.timeScale(4.0); },}
+        });
+        kennzahlTl1
+            .from(".kennzahl-1 .kennzahl-img", { scale: 0.8, ease: "back.out(6)" })
+            .from(".kennzahl-1 .kennzahl-text-wrapper", { autoAlpha: 0, y: 40, ease: "power4.out" }, "-=1.6")
+            .from(".kennzahl-1 .number", { duration: 3, marginBottom: 30, innerHTML: "0", roundProps: "innerHTML", ease: "power4.out", stagger: { from: "center", amount: 0.25 } }, "-=2")
+        
+        let kennzahlTl2 = gsap.timeline({
+            defaults:{ duration: 1.8 },
+            scrollTrigger: { trigger: '.kennzahl-2', start: "center 90%", end: '+=150px', toggleActions: "play none none reverse", onEnter: () => { kennzahlTl2.timeScale(1.0); }, onEnterBack: () => { kennzahlTl2.timeScale(4.0); },}
+        });
+        kennzahlTl2
+            .from(".kennzahl-2 .kennzahl-img", { scale: 0.8, ease: "back.out(6)" })
+            .from(".kennzahl-2 .kennzahl-text-wrapper", { autoAlpha: 0, y: 40, ease: "power4.out" }, "-=1.6")
+            .from(".kennzahl-2 .number", { duration: 3, marginBottom: 30, innerHTML: "0", roundProps: "innerHTML", ease: "power4.out", stagger: { from: "center", amount: 0.25 } }, "-=2")
+        
+        let kennzahlTl3 = gsap.timeline({
+            defaults:{ duration: 1.8 },
+            scrollTrigger: { trigger: '.kennzahl-3', start: "center 90%", end: '+=150px', toggleActions: "play none none reverse", onEnter: () => { kennzahlTl3.timeScale(1.0); }, onEnterBack: () => { kennzahlTl3.timeScale(4.0); },}
+        });
+        kennzahlTl3
+            .from(".kennzahl-3 .kennzahl-img", { scale: 0.8, ease: "back.out(6)" })
+            .from(".kennzahl-3 .kennzahl-text-wrapper", { autoAlpha: 0, y: 40, ease: "power4.out" }, "-=1.6")
+            .from(".kennzahl-3 .number", { duration: 3, marginBottom: 30, innerHTML: "0", roundProps: "innerHTML", ease: "power4.out", stagger: { from: "center", amount: 0.25 } }, "-=2")
+
+
+        ////////////////////////////
+        // Wikungskette Animation
+        ////////////////////////////
+        
+        // Path Animation
+        var wirkungsketteTl = gsap.timeline({defaults: {duration: 1, repeat:0},
+            scrollTrigger: {
+                markers: false,
+                trigger: "#wirkungskette",
+                scrub: 0.5,
+                start: "top 55%",
+                end: "bottom 90%",
+            }
+        })
+        
+        var d = 1;
+        var maxI = 0;
+        $($("#wirkungskette-mobile-svg g#path").children().get().reverse()).each(function(i, child){
+          wirkungsketteTl.to($(child), d, {"opacity":1});
+        });
+        
+
+        // Icons Animation
+        let iconAniTime = 1;
+        let textAniTime = 1.2;
+        
+        let wirkungsIconTl1 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "top 70%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl1.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl1.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-1", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-1", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl2 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "23% 68%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl2.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl2.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-2", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-2", { duration: textAniTime, /*x: 200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl3 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "41% 58%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl3.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl3.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-3", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-3", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl4 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "60% 69%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl4.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl4.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-4", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-4", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl5 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "73% 52%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl5.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl5.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-5", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)" })
+        .from(".wk-text-box-5", { duration: textAniTime, /*x: 200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" );
+        
+        let wirkungsIconTl6 = gsap.timeline({
+            scrollTrigger: { trigger: '#wirkungskette', start: "bottom 86%", end: '+=100px', toggleActions: "play none none reverse", onEnter: () => { wirkungsIconTl6.timeScale(1.0); }, onEnterBack: () => { wirkungsIconTl6.timeScale(3.0); }}
+        })
+        .from(".wirkungskette-icon-6", { duration: iconAniTime, scale: 0, transformOrigin: "center", ease: "back.out(2)"})
+        .from(".wk-text-box-6", { duration: textAniTime, /*x: -200,*/ opacity: 0, ease: "power2.out"}, "-=0.6" )
+
+
+            
+    }
+});
+
+
+} )( jQuery );
+</script>
