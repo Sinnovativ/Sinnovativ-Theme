e9b86e34281de575cda4c574247c86456f43bd86
Martin <martin.hertig@gmail.com>
2021-02-25 14:16:06 +0100

    remove recent posts from 404 page

--- 404.php
+++ 404.php
@@ -39,32 +39,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 							<?php get_search_form(); ?>
 
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-
-								<div class="widget widget_categories">
-
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'understrap' ); ?></h2>
-
-									<ul>
-										<?php
-										wp_list_categories(
-											array(
-												'orderby'    => 'count',
-												'order'      => 'DESC',
-												'show_count' => 1,
-												'title_li'   => '',
-												'number'     => 10,
-											)
-										);
-										?>
-									</ul>
-
-								</div><!-- .widget -->
-
-							<?php endif; ?>
-
 							<?php
 
 							/* translators: %1$s: smiley */
