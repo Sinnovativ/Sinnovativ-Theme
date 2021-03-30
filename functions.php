f04930616b1b3a34e817a0b9eb322fdb808e2dfc
Martin <martin.hertig@gmail.com>
2021-03-30 11:14:27 +0200

    add og tags to head

--- functions.php
+++ functions.php
@@ -250,3 +250,33 @@ function cstm_display_product_category()
     echo '<div class="product_size">Grösse: ' . $size . '</div>';
  }
 }
+
+
+//Adding the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+        return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+    }
+add_filter('language_attributes', 'add_opengraph_doctype');
+
+//Lets add Open Graph Meta Info
+
+function insert_fb_in_head() {
+    global $post;
+    if ( !is_singular()) //if it is not a post or a page
+        return;
+        echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+        echo '<meta property="og:site_name" content="'.get_bloginfo('name').'"/>';
+    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+        $default_image= get_bloginfo('url')."/wp-content/uploads/2020/12/drahtesel_logo.png"; //replace this with a default image on your server or an image in your media library
+        echo '<meta property="og:image" content="' . $default_image . '"/>';
+    }
+    else{
+        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+    }
+    echo "
+";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
