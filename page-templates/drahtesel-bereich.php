697582eb1b7d65a53d34990049ea22d313ce6681
Martin <martin.hertig@gmail.com>
2021-03-30 10:33:53 +0200

    news statt was uns bewegt

--- page-templates/drahtesel-bereich.php
+++ page-templates/drahtesel-bereich.php
@@ -61,7 +61,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                         <p><?php the_sub_field('zeiten'); ?></p>
                     <?php endwhile; ?>
                 </div>
-            <?php endif; ?>          
+            <?php endif; ?>
 
             <div class="container mt-md-3 mt-lg-5">
 
@@ -82,8 +82,8 @@ $container = get_theme_mod( 'understrap_container_type' );
         <?php if(get_field('news_anzeigen')): ?>
 
             <div class="container news-container">
-                <h2>Was uns bewegt</h2>
-        
+                <h2>News</h2>
+
                 <?php
                 // Show latest posts and only selectet categorie
                 $category_ID = get_field('bereich');
@@ -96,14 +96,14 @@ $container = get_theme_mod( 'understrap_container_type' );
                 );
 
                 ?>
-        
-        
+
+
                 <div class="row posts-overview row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-        
+
                     <?php while ( $the_query ->have_posts() ) : $the_query -> the_post(); ?>
-        
+
                       <?php
-        
+
                       /*
                        * Include the Post-Format-specific template for the content.
                        * If you want to override this in a child theme, then include a file
@@ -111,19 +111,19 @@ $container = get_theme_mod( 'understrap_container_type' );
                        */
                       get_template_part( 'loop-templates/content', get_post_format() );
                       ?>
-        
+
                     <?php endwhile;
-        
+
                     wp_reset_postdata();
                     ?>
                 </div>
-        
+
                 <div class="col-md-12">
                     <div class="mehr-news center">
                       <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="btn btn-outline-primary"><?php _e( 'Mehr News', 'Drahtesel Startseite');  ?></a>
-                    </div>                
+                    </div>
                 </div>
-        
+
             </div><!-- .container end -->
         <?php endif; ?>
 
